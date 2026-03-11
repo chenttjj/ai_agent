cf937a47b15c4a733d67582b52a21876c943705b
chenttjj <chentj0409@gmail.com>
2026-03-11 16:21:36 +0800

    feat: replace static greeting with dynamic OpenAI LLM interaction and load environment variables.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai-agent!")
+from openai.types.chat.chat_completion_stream_options_param import ChatCompletionStreamOptionsParam
+from openai import responses
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+def main():
+  llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.7)
+  response = llm.invoke("Hello, how are you?")
+  print(response.content)
 
 if __name__ == "__main__":
     main()
